4b83ff7ff87a4b3a39ddcba07d82ed8cac4e23fd
Wuriyanto <wuriyanto48@yahoo.co.id>
2021-12-10 09:36:38 +0700

    add database header

---
+++ src/database.c
@@ -0,0 +1,95 @@
+#include "database.h"
+
+static struct database* DATABASE;
+
+int init_database() 
+{
+    DATABASE = (struct database*) malloc(sizeof(*DATABASE));
+    if (DATABASE == NULL)
+        return -1;
+    return 0;
+}
+
+void destroy_database()
+{
+    if (DATABASE != NULL)
+        free((void*) DATABASE);
+}
+
+void destroy_element(struct element* d)
+{
+    if (d != NULL)
+    {
+        free((void*) d->key);
+        free((void*) d->val);
+    }
+}
+
+unsigned int element_hash(const char* key) 
+{
+    unsigned int hashval;
+    for (hashval = 0; *key != '\0'; key++)
+        hashval = *key + 31 * hashval;
+    return (hashval % HASH_SIZE);
+}
+
+struct element* element_get(const char* key)
+{
+    struct element* data;
+    for (data = DATABASE->element_table[element_hash(key)]; data != NULL; data = data->next) {
+        printf("iter\n");
+        if (strcmp(key, data->key) == 0)
+            return data;
+    }
+    return NULL;
+}
+
+struct element* element_insert(const char* key, const char* val)
+{
+    if (DATABASE->size >= HASH_SIZE)
+        return NULL;
+        
+    struct element* res;
+    unsigned int hashval;
+
+    if ((res = element_get(key)) == NULL) { // not found
+        res = (struct element*) malloc(sizeof(*res));
+        if (res == NULL || (res->key = element_dup(key)) ==  NULL)
+            return NULL;
+        hashval = element_hash(key);
+        res->next = DATABASE->element_table[hashval];
+        DATABASE->element_table[hashval] = res;
+    } else // key found
+        free((void*) res->val);
+    
+    if ((res->val = element_dup(val)) == NULL)
+        return NULL;
+    DATABASE->size++;
+    return res;
+}
+
+void element_delete(const char* key) 
+{
+    struct element* res;
+    unsigned int hashval;
+
+    if ((res = element_get(key)) != NULL) {
+        hashval = element_hash(key);
+        DATABASE->element_table[hashval] = NULL;
+        destroy_element(res);
+        DATABASE->size--;
+    }
+}
+
+char* element_dup(const char* s)
+{
+    char* res;
+    res = (char*) malloc(strlen(s) + 1); // + 1 for NULL terminator '\0'
+    if (res != NULL)
+        strcpy(res, s);
+    return res;
+}
+
+size_t element_size() {
+    return DATABASE->size;
+}
